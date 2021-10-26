0d5c8fc8a021b4ae4593234ce86a5d3903f080bc
Ruby Amyeen <rsamyeen@gmail.com>
2021-10-25 21:13:19 -0700

    first commit 10/25/21

---
+++ Test/Test/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Test
+//
+//  Created by Ruby Amyeen on 10/24/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
